cb0818fcf1f3b53ce6bfc5e92ae36e71cd29ab1a
SunitaGirl <sramnari@gmail.com>
2016-10-28 17:39:04 -0400

    Adding background pic and bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+
+{	
+	var question1 = 
+	{
+		question: "Who was the first Disney princess based on a real historical figure?",
+		answer: "Pocahontas",
+		possible: ["Mulan", "Snow White", "Tiana", "Pocahontas"],
+		boolean: [false,false,false,true]
+	};
+
+	var question2 = 
+	{
+		question: "Which is the only Disney animated film with a main character that doesn’t speak?",
+		answer: "Dumbo",
+		possible: ["Robin Hood", "Dumbo", "Pinocchio", "Fantasia"],
+		boolean: [false,true,false,false]
+	};
