5839b11ba335ab1b9ee0374ee31dd2e4bd1df6ea
SunitaGirl <sramnari@gmail.com>
2016-10-28 21:30:32 -0400

    Added vars for questions with objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function()
 	{
 		question: "Who was the first Disney princess based on a real historical figure?",
 		answer: "Pocahontas",
-		possible: ["Mulan", "Snow White", "Tiana", "Pocahontas"],
+		choices: ["Mulan", "Snow White", "Tiana", "Pocahontas"],
 		boolean: [false,false,false,true]
 	};
 
@@ -13,6 +13,23 @@ $(document).ready(function()
 	{
 		question: "Which is the only Disney animated film with a main character that doesn’t speak?",
 		answer: "Dumbo",
-		possible: ["Robin Hood", "Dumbo", "Pinocchio", "Fantasia"],
+		choices: ["Robin Hood", "Dumbo", "Pinocchio", "Fantasia"],
 		boolean: [false,true,false,false]
 	};
+
+	var question3 = 
+	{
+		question: "Which actor supplied the voice of Mushu in Mulan?",
+		answer: "Eddie Murphy",
+		choices: ["Tom Cruise", "Brad Pitt", "Eddie Murphy", "Nicholas Cage"],
+		boolean: [false,false,true,false]
+	};
+
+	var question4 = 
+	{
+		question: "In Aladdin, what does Jasmine steal from the Marketplace?",
+		answer: "Apple",
+		choices: ["Apple", "Bread", "Jewelry", "Grapes"],
+		boolean: [true,false,false,false]
+	};
+
