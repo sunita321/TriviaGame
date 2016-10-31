59dbbec6f0b00d0d18fc6f615a5386f24c79d256
SunitaGirl <sramnari@gmail.com>
2016-10-30 21:42:34 -0400

    Added logic for game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,60 @@
 //Declare Quiz Questions variables
-	var question1 = 
+	var question0 = 
 	{
 		question: "Who was the first Disney princess based on a real historical figure?",
 		answer: "Pocahontas",
 		choices: ["Mulan", "Snow White", "Tiana", "Pocahontas"],
-		correctAnswer: 4
+		correctAnswer: 3
 	};
 
-	var question2 = 
+	var question1 = 
 	{
 		question: "Which is the only Disney animated film with a main character that doesn’t speak?",
 		answer: "Dumbo",
 		choices: ["Robin Hood", "Dumbo", "Pinocchio", "Fantasia"],
+		correctAnswer: 1
+	};
+
+	var question2 = 
+	{
+		question: "Which actor supplied the voice of Mushu in Mulan?",
+		answer: "Eddie Murphy",
+		choices: ["Tom Cruise", "Brad Pitt", "Eddie Murphy", "Nicholas Cage"],
 		correctAnswer: 2
 	};
 
 	var question3 = 
 	{
-		question: "Which actor supplied the voice of Mushu in Mulan?",
-		answer: "Eddie Murphy",
-		choices: ["Tom Cruise", "Brad Pitt", "Eddie Murphy", "Nicholas Cage"],
-		correctAnswer: 3
-	};
-
-	var question4 = 
-	{
 		question: "In Aladdin, what does Jasmine steal from the Marketplace?",
 		answer: "Apple",
 		choices: ["Apple", "Bread", "Jewelry", "Grapes"],
-		correctAnswer: 1
+		correctAnswer: 0
 	};
+
+
+//Array of questions
+var QuestionsArray = [question0, question1, question2, question3];
+
+var indexQuestion = 0;
+
 
 //Game scores
 var gameScores = 
 {
-	answeredCorrectly: 0,
-	answeredIncorrectly: 0,
-	unanswered: 0
+	answeredCorrect: 0,
+	answeredWrong: 0,
+	missed: 0
 };
 
 //Display Question
 
 	function displayQuestion()
 	{
-		$("#question").html("<h3>" + question1.question + "</h3>");
-		$("#button0").text(question1.choices[0]);
-		$("#button1").text(question1.choices[1]);
-		$("#button2").text(question1.choices[2]);
-		$("#button3").text(question1.choices[3]);
+		$("#question").html("<h3>" + QuestionsArray[indexQuestion].question + "</h3>");
+		$("#button0").text(QuestionsArray[indexQuestion].choices[0]);
+		$("#button1").text(QuestionsArray[indexQuestion].choices[1]);
+		$("#button2").text(QuestionsArray[indexQuestion].choices[2]);
+		$("#button3").text(QuestionsArray[indexQuestion].choices[3]);
 
 	}
 
@@ -59,25 +66,36 @@ $(document).ready(function()
 });
 
 //User input 
-function check_answer () {
+$('.btn').click(function() 
+{
+	var userButtonValue = ($(this).attr("data-value"));
+	console.log(userButtonValue);
 
-	$(".options").on("click" , function(){
-
-		var userInput = $(this);
-		user_check = user_guess.data("boolean");
-
-		console.log(user_guess);
-		console.log(user_check);
-
-		if (user_check == true) {
-
-			console.log("hey");
-			right_answer();
-
-		} else {
-			console.log("bye");
-			wrong_answer();
-		}
-
-
+	if (userButtonValue == QuestionsArray[indexQuestion].correctAnswer)
+	{
+		gameScores.answeredCorrect ++;
+		console.log("correct answer " + gameScores.answeredCorrect);
 	}
+
+	else
+	{
+		gameScores.answeredWrong ++;
+		console.log("wrong answer " + gameScores.answeredWrong);
+	}
+
+
+	indexQuestion++;
+
+	if (indexQuestion < QuestionsArray.length)
+	{
+		displayQuestion();
+	}
+	else
+	{
+		$("#score").html("<div>"+ "Correct Guesses: " + gameScores.answeredCorrect +"</div>" + "<div>"+ "Wrong Guesses: " + gameScores.answeredWrong +"</div>");
+		
+	}
+	
+
+});
+
